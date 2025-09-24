7e70f07df1c737091ea40715bafaa575be173a1a
Adair Hernandez <adheva05@gmail.com>
2025-09-24 11:45:15 -0600

    v1.2

--- pract2.py
+++ pract2.py
@@ -2,80 +2,20 @@ import re
 
 # Ejercicio 1
 
-texto = "Aprender Python es divertido"
-patron = re.compile("Python")
+texto = "Ana_ 123, Pedro!, @Luis45, Maria99##, #Sofía"
+patron = r"[\d\W_]+"
 
-if patron.search(texto):
-    print("¡Patrón encontrado!")
-    print(f"Texto encontrado: {patron.search(texto).group()}")
-else:
-    print("Patrón no encontrado.")
+print(re.sub(patron, ", ", texto))
 
 # Ejercicio 2
 
-texto = "Hola, bienvenido"
-patron = r"^Hola"
+texto = '192.168.1.1 - - [16/Sep/2025:10:30:00] "GET /index.html HTTP/1.1" 200 1024'
+patron_ip = r"[\d{1,3}\.]+"
+patron_fecha = r"\d{2}/[A-Za-z]{3}/\d{4}:[\d{2}:]+"
+patron_GET = r"GET"
 
-if re.match(patron, texto):
-    print(f"El texto comienza con 'Hola' {re.match(patron, texto)}")
-else:
-    print(f"El texto no comienza con 'Hola' {re.match(patron, texto)}")
+ip = re.search(patron_ip, texto).group()
+fecha = re.search(patron_fecha, texto).group()
+metodo = re.search(patron_GET, texto).group()
 
-
-# Ejercicio 3
-
-frase = "Hoy es un gran dia"
-patron = r"dia$"
-
-if re.search(patron, frase):
-    print(f"La frase termina con 'dia' {re.search(patron, frase)}")
-else:
-    print(f"La frase no termina con 'dia' {re.search(patron, frase)}")
-
-
-# Ejercicio 4
-
-texto = "Tengo 2 perros, 3 gatos y 1 tortuga"
-patron = r"\d+"
-
-if re.search(patron, texto):
-    print(f"Se encontraron números: {re.findall(patron, texto)}")
-else:
-    print("No se encontraron números.")
-
-
-# Ejercicio 5
-cadena = "Python 3 es genial"
-patron = r"\w+"
-
-palabras = re.findall(patron, cadena)
-print(f"Palabras encontradas: {palabras}")
-
-
-# Ejercicio 6
-texto = "Ese producto es malo y feo"
-patron = r"\b(malo|feo)\b"
-
-if re.search(patron, texto):
-    resultado = re.sub(patron, "***", texto)
-    print(f"Texto modificado: {resultado}")
-else:
-    print("No se encontraron palabras ofensivas.")
-
-
-# Ejercicio 7
-
-patron = r"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{3,})"
-
-lista_correos = ["usuario@gmail.com", "usuario@mail", "usuario@dominio.org"]
-
-correos_validos = [correo for correo in lista_correos if re.match(patron, correo)]
-
-print(f"Correos válidos: {correos_validos}")
-
-# Ejercicio 8
-texto = "Hoy es 16/09/2025 y mañana sera 17/09/2025"
-patron = r"\d{2}/\d{2}/\d{4}"
-
-fechas = re.findall(patron, texto)
-print(f"Fechas encontradas: {fechas}")
+print(f"IP: {ip} \nFecha y hora: {fecha} \nMétodo: {metodo}")
